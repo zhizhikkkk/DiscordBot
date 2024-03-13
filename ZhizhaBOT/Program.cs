3e390417cc3eb504e6a0a5278120c3a95d7e6039
zhizhikkkk <zhanelmamyt@gmail.com>
2024-03-13 15:59:02 +0600

    Create | Connect | SetUp

--- ZhizhaBOT/Program.cs
+++ ZhizhaBOT/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
 using System.Threading.Tasks;
 
 namespace ZhizhaBOT
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+        static async Task Main(string[] args)
         {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordCondig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+
+            };
+
+            Client = new DiscordClient(discordCondig);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
         }
     }
 }
